bc4ae078a2ed76ea1576e9b17872beb07c50bddb
Ishaan Ohri <ishaanohri@Ishaans-MacBook-Pro.local>
2020-11-24 23:51:00 +0530

    setup cron

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const moment = require('moment');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+var CronJob = require('cron').CronJob;
+
+var job = new CronJob('* * * * * *', function() {
+  console.log('You will see this message every second');
+}, null, true, 'America/Los_Angeles');
+
 app.get('/', (req, res) => {
     res.send({
         success: true,
@@ -15,4 +21,5 @@ app.get('/', (req, res) => {
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
+    job.start();
 });
