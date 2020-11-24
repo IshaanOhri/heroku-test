85e6239a9610e94a45506324852e4491181a6d83
Ishaan Ohri <ishaanohri@Ishaans-MacBook-Pro.local>
2020-11-24 23:56:39 +0530

    added node-fetch

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const moment = require('moment');
+const fetch = require('node-fetch');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,6 +10,9 @@ var CronJob = require('cron').CronJob;
 
 var job = new CronJob('* * * * * *', function() {
   console.log('You will see this message every second');
+  fetch('https://heroku-time-test.herokuapp.com/')
+    .then(res => res.text())
+    .then(body => console.log(body));
 }, null, true, 'America/Los_Angeles');
 
 app.get('/', (req, res) => {
