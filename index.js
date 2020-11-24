5d5b170d3a0fa0e434a4dafe5d1d76c59bdebc8a
Ishaan Ohri <ishaanohri@Ishaans-MacBook-Pro.local>
2020-11-24 23:31:28 +0530

    added basic code

--- index.js
+++ index.js
@@ -2,3 +2,17 @@ const express = require('express');
 const mongoose = require('mongoose');
 const moment = require('moment');
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get((req, res) => {
+    res.send({
+        success: true,
+        message: 'Hey there',
+        timestamp: moment().format()
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+});
